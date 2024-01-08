2e03207349fc954ea7b19b7bbc4367c0fc059337
Nathan Grenier <nathangrenier01@gmail.com>
2024-01-08 15:11:31 -0500

    added git build info to shuffleboard
    also added field image during simulation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,17 +6,55 @@ package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.util.Map;
 
 public class Robot extends TimedRobot {
   private final XboxController m_controller = new XboxController(0);
   private final Drivetrain m_swerve = new Drivetrain();
 
+  private final Field2d m_field = new Field2d();
+
   // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
   private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(3);
   private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(3);
   private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
+
+  @Override
+  public void robotInit() {
+    // Default table is started automatically
+    NetworkTableInstance netTable = NetworkTableInstance.getDefault();
+
+    if (RobotBase.isSimulation()) {
+      SmartDashboard.putData("Field", m_field);
+    }
+
+    // Shuffleboard
+    ShuffleboardLayout metadata_tab = Shuffleboard.getTab("metadata")
+        .getLayout("GitBuildConstants", BuiltInLayouts.kList)
+        .withSize(3, 2)
+        .withProperties(Map.of("Label position", "LEFT"));
+
+    metadata_tab.add("GIT_BRANCH", BuildConstants.GIT_BRANCH);
+    metadata_tab.add("BUILD_DATE", BuildConstants.BUILD_DATE);
+    metadata_tab.add("Uncommited Changes", BuildConstants.DIRTY == 1 ? "Yes" : "No"); 
+  
+  }
+
+  public void robotPeriodic() {
+    if (RobotBase.isSimulation()) {
+      // TODO: Add when pose is implemented
+      // m_field.setRobotPose(m_swerve.getPose());
+    }
+  }
 
   @Override
   public void autonomousPeriodic() {
@@ -32,24 +70,21 @@ public class Robot extends TimedRobot {
   private void driveWithJoystick(boolean fieldRelative) {
     // Get the x speed. We are inverting this because Xbox controllers return
     // negative values when we push forward.
-    final var xSpeed =
-        -m_xspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftY(), 0.02))
-            * Drivetrain.kMaxSpeed;
+    final var xSpeed = -m_xspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftY(), 0.02))
+        * Drivetrain.kMaxSpeed;
 
     // Get the y speed or sideways/strafe speed. We are inverting this because
     // we want a positive value when we pull to the left. Xbox controllers
     // return positive values when you pull to the right by default.
-    final var ySpeed =
-        -m_yspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftX(), 0.02))
-            * Drivetrain.kMaxSpeed;
+    final var ySpeed = -m_yspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftX(), 0.02))
+        * Drivetrain.kMaxSpeed;
 
     // Get the rate of angular rotation. We are inverting this because we want a
     // positive value when we pull to the left (remember, CCW is positive in
     // mathematics). Xbox controllers return positive values when you pull to
     // the right by default.
-    final var rot =
-        -m_rotLimiter.calculate(MathUtil.applyDeadband(m_controller.getRightX(), 0.02))
-            * Drivetrain.kMaxAngularSpeed;
+    final var rot = -m_rotLimiter.calculate(MathUtil.applyDeadband(m_controller.getRightX(), 0.02))
+        * Drivetrain.kMaxAngularSpeed;
 
     m_swerve.drive(xSpeed, ySpeed, rot, fieldRelative, getPeriod());
   }
