1aabd15bbfc889e79f8d79eb7c765c8cfc2fcfde
NathanGrenier <nathangrenier01@gmail.com>
2024-01-15 15:08:24 -0500

    fix: git user change is no longer global

--- switch-gituser/testUser.sh
+++ switch-gituser/testUser.sh
@@ -5,7 +5,7 @@ USERNAME="Username1"
 EMAIL="email1@example.com"
 
 # Configure git with the new credentials
-git config --global user.name "$USERNAME"
-git config --global user.email "$EMAIL"
+git config user.name "$USERNAME"
+git config user.email "$EMAIL"
 
 echo "Git user has been set to: $USERNAME ($EMAIL)"
