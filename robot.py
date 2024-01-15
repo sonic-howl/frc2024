4ef4145612a0864c770d017915c1d4c765409052
NathanGrenier <nathangrenier01@gmail.com>
2024-01-15 14:13:44 -0500

    refactor: added timed robot functions

--- robot.py
+++ robot.py
@@ -1,1 +1,37 @@
-# TODO: insert robot code here
+import wpilib
+
+
+class MyRobot(wpilib.TimedRobot):
+  def robotInit(self):
+    """
+    This function is called upon program startup and
+    should be used for any initialization code.
+    """
+
+  def autonomousInit(self):
+    """This function is run once each time the robot enters autonomous mode."""
+
+  def autonomousPeriodic(self):
+    """This function is called periodically during autonomous."""
+
+  def teleopInit(self):
+    """This function is called once each time the robot enters teleoperated mode."""
+
+  def teleopPeriodic(self):
+    """This function is called periodically during teleoperated mode."""
+
+  def testInit(self):
+    """This function is called once each time the robot enters test mode."""
+
+  def testPeriodic(self):
+    """This function is called periodically during test mode."""
+
+  # def simulationInit(self):
+  #   """This function is called once each time the robot enters simulation mode."""
+
+  # def simulationPeriodic(self):
+  #   """This function is called periodically during simulation mode."""
+
+
+if __name__ == "__main__":
+  wpilib.run(MyRobot)
