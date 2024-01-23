c6114db21ccf9b23830a20082201970599dfc1f2
NathanGrenier <nathangrenier01@gmail.com>
2024-01-23 10:56:15 -0500

    refactor: moved deploy artifact code to seperate file

--- robot.py
+++ robot.py
@@ -1,7 +1,7 @@
-import ntcore
 import wpilib
 import wpilib.deployinfo
-from wpilib.shuffleboard import BuiltInLayouts, Shuffleboard
+
+from shuffleboard import addDeployArtifacts
 
 
 class MyRobot(wpilib.TimedRobot):
@@ -10,21 +10,8 @@ class MyRobot(wpilib.TimedRobot):
     This function is called upon program startup and
     should be used for any initialization code.
     """
-    if wpilib.RobotBase.isReal():  # getDeployData() returns None during simulation
-      deployArtifacts = wpilib.deployinfo.getDeployData()
-      (
-        buildArtifacts_layout := Shuffleboard.getTab("metadata")
-        .getLayout("DeployArtifacts", BuiltInLayouts.kList)
-        .withSize(3, 2)
-        .withProperties({"Label position": ntcore._ntcore.Value.makeString("LEFT")})
-      )
-
-      buildArtifacts_layout.add("GIT_BRANCH", deployArtifacts["git-branch"])
-      buildArtifacts_layout.add("DEPLOY_DATE", deployArtifacts["deploy-date"])
-      buildArtifacts_layout.add(
-        "Uncommited Changes",
-        "Yes" if "dirty" in deployArtifacts["git-desc"].split("-") else "No",
-      )
+    # Add the deploy artifacts to the shuffleboard
+    addDeployArtifacts()
 
   def autonomousInit(self):
     """This function is run once each time the robot enters autonomous mode."""
