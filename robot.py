8eb22ab12692d3afa29a6e71608668ea152b3d39
NathanGrenier <56941028+NathanGrenier@users.noreply.github.com>
2024-02-05 21:41:11 -0500

    Mentor/tests (#26)
    * Feature/deploy artifacts (#24)
    
    * wip: display deploy artifacts to shuffleboard
    
    * uncommented as it was causing test issues
    
    * autogenerated when running robotpy deploy
    
    * wip: getDeployData() returns null during simulation
    
    * fixed typo
    
    * forgot to add robot.py changes
    
    * wip: index out of range on slipt()
    
    * fix: parse git-descript to know if there are uncommited changes
    
    * refactor: moved deploy artifact code to seperate file
    
    ---------
    
    Co-authored-by: portnord <45200621+Portnord@users.noreply.github.com>
    
    * feat: added sample test and documentation on how to run test and coverage reports
    
    * wip: added tests and coverage to workflow
    
    * build: updated ropotpy_version
    
    * feat: add link to coverage html in PR
    
    * testing different directory
    
    * fix: commented broken action (to be implemented later)
    
    * made test import more verbose
    
    * renamed sample test
    
    * updated robotpy version
    
    ---------
    
    Co-authored-by: portnord <45200621+Portnord@users.noreply.github.com>

--- robot.py
+++ robot.py
@@ -1,4 +1,7 @@
 import wpilib
+import wpilib.deployinfo
+
+from shuffleboard import addDeployArtifacts
 
 
 class MyRobot(wpilib.TimedRobot):
@@ -7,6 +10,8 @@ class MyRobot(wpilib.TimedRobot):
     This function is called upon program startup and
     should be used for any initialization code.
     """
+    # Add the deploy artifacts to the shuffleboard
+    addDeployArtifacts()
 
   def autonomousInit(self):
     """This function is run once each time the robot enters autonomous mode."""
