877246ede961fcf30bcaf62ea79b9c6a70293fae
magnite5 <87040030+magnite5@users.noreply.github.com>
2024-02-05 19:57:55 -0500

    moved shuffleboard.py into /src

--- src/tests/pyfrc_test.py
+++ src/tests/pyfrc_test.py
@@ -2,5 +2,9 @@
     This test module imports tests that come with pyfrc, and can be used
     to test basic functionality of just about any robot.
 """
-
-from pyfrc.tests import *  # noqa: F403
+from pyfrc.tests import (  # noqa: F401
+  test_autonomous,
+  test_disabled,
+  test_operator_control,
+  test_practice,
+)
