60a05602be8f26e5340fffcd4d22384e309b9245
magnite5 <87040030+magnite5@users.noreply.github.com>
2024-02-21 18:11:10 -0500

    Adding Camera Constants (Focal lengths and positions)

---
+++ src/constants/CameraConstants.py
@@ -0,0 +1,14 @@
+import RobotConstants
+
+# Approximate measured Focal Lengths (FL is Focal length), 640 x 480p
+MSLifeFL = 625
+LogitechFL = 800
+
+
+class kRearCamera:
+  x = -RobotConstants.frame_length / 2
+  y = 0
+  z = 12
+  Elevation = 0
+  Rotation = 0
+  FL = MSLifeFL
