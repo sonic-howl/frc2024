bea51539ecf563b6df8c1b1af84afb11cdf96ed0
magnite5 <87040030+magnite5@users.noreply.github.com>
2024-02-22 19:52:10 -0500

    Bot will now stop when limit switches are activated

--- src/launcher.py
+++ src/launcher.py
@@ -1,4 +1,5 @@
 import rev
+import wpilib
 
 
 class Launchers:
@@ -7,11 +8,15 @@ class Launchers:
 
     self.motor2 = rev.CANSparkMax(11, rev.CANSparkLowLevel.MotorType.kBrushless)
 
-    self.elevator = rev.CANSparkMax(12, rev.CANSparkLowLevel.MotorType.kBrushless)
+    self.elevator = rev.CANSparkMax(12, rev.CANSparkLowLevel.MotorType.kBrushed)
 
     self.encoder1 = self.motor1.getEncoder()
 
     self.encoder2 = self.motor2.getEncoder()
+
+    self.upperswitch = wpilib.DigitalInput(0)
+
+    self.lowerswitch = wpilib.DigitalInput(1)
 
   def stop(self):
     self.motor1.set(0.0)
@@ -28,16 +33,24 @@ class Launchers:
       #  self.launcher_RPM1 = RPMInfo_layout.add( "Launcher_RPM1", launcher.encoder1.getVelocity()).getEntry()
       #  self.launcher_RPM2 = RPMInfo_layout.add("Launcher_RPM2", launcher.encoder2.getVelocity()).getEntry()
 
-      if self.encoder1.getVelocity() < -4501.0 and self.encoder2.getVelocity() > 4501.0:
+      if self.encoder1.getVelocity() < -4500.0 and self.encoder2.getVelocity() > 4500.0:
         pass
       # feed ring into launcher
 
   def unjams(self):
-    self.motor1.set(0.51)
-    self.motor2.set(-0.51)
+    self.motor1.set(0.5)
+    self.motor2.set(-0.5)
 
   def elevate(self, elevate: float):
-    if abs(elevate) <= 0.05:
+    isGoingDown = elevate <= 0
+    is_upper_switch_on = self.upperswitch.get()
+    is_lower_switch_on = self.lowerswitch.get()
+
+    if abs(elevate) <= 0.05():
+      self.stop()
+    elif isGoingDown and is_lower_switch_on:
+      self.stop()
+    elif not isGoingDown and is_upper_switch_on:
       self.stop()
     else:
       self.elevator.set(elevate)
